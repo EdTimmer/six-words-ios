5c80f70730841e641301cd05002218a13a2683ae
Ed Timmer <edtimmer@gmail.com>
2022-12-07 22:17:07 -0500

    Initial Commit

---
+++ Six Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Six Words
+//
+//  Created by Ed Timmer on 12/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
